ca081a0f2d4e18c48607c6970176382d953c4ff1
Matthew Sinex <matthewsinex@gmail.com>
2013-01-27 18:08:27 -0700

    initial commit

---
+++ IOC Timer.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace IOCTimer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.btnContinue = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.lblRoundTitle = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.Font = new System.Drawing.Font("Calibri", 200.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTimer.Location = new System.Drawing.Point(157, 38);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(748, 326);
+            this.lblTimer.TabIndex = 0;
+            this.lblTimer.Text = "10:00";
+            // 
+            // btnContinue
+            // 
+            this.btnContinue.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnContinue.Font = new System.Drawing.Font("Calibri", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnContinue.Location = new System.Drawing.Point(404, 367);
+            this.btnContinue.Name = "btnContinue";
+            this.btnContinue.Size = new System.Drawing.Size(214, 89);
+            this.btnContinue.TabIndex = 1;
+            this.btnContinue.Text = "Start";
+            this.btnContinue.UseVisualStyleBackColor = true;
+            this.btnContinue.Click += new System.EventHandler(this.btnContinue_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnReset.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(459, 471);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(109, 35);
+            this.btnReset.TabIndex = 2;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 500;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // lblRoundTitle
+            // 
+            this.lblRoundTitle.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblRoundTitle.Font = new System.Drawing.Font("Calibri", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRoundTitle.Location = new System.Drawing.Point(204, 27);
+            this.lblRoundTitle.Name = "lblRoundTitle";
+            this.lblRoundTitle.Size = new System.Drawing.Size(662, 53);
+            this.lblRoundTitle.TabIndex = 3;
+            this.lblRoundTitle.Text = "Poetry Commentary";
+            this.lblRoundTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblRoundTitle.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1022, 537);
+            this.Controls.Add(this.lblRoundTitle);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnContinue);
+            this.Controls.Add(this.lblTimer);
+            this.Name = "Form1";
+            this.Text = "IOC Timer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTimer;
+        private System.Windows.Forms.Button btnContinue;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label lblRoundTitle;
+    }
+}
+
---
+++ IOC Timer.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace IOCTimer
+{
+    public partial class Form1 : Form
+    {
+        private TimeSpan duration, remaining;
+        private DateTime startTime;
+        private int round = 0;
+        private int timerTickCount = 0;
+        private int TOTAL_MINUTES = 10;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            duration = new TimeSpan(0, TOTAL_MINUTES, 1);
+        }
+
+        private void btnContinue_Click(object sender, EventArgs e)
+        {
+            if (round == 0)
+            {
+                lblRoundTitle.Text = "Poetry Commentary";
+                startTime = DateTime.Now;
+                timer1.Enabled = true;
+                round = 1;
+                btnContinue.Text = "Continue";
+                lblRoundTitle.Visible = true;
+            }
+
+            else
+            {
+                lblRoundTitle.Text = "Interview";
+                duration = new TimeSpan(0, TOTAL_MINUTES, 1);
+                btnContinue.Enabled = false;
+                startTime = DateTime.Now;
+                timer1.Enabled = true;
+                timer2.Enabled = false;
+                this.BackColor = SystemColors.Control;
+                lblTimer.Text = "10:00";
+                round = 2;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //calcuates and displays the remaining time
+            remaining = duration - (DateTime.Now - startTime);
+
+            //two Minutes for Questions
+            if (round == 1 && lblTimer.Text == "02:00")
+            {
+                twoMinuteWarning();
+                lblRoundTitle.Text = "Poetry Commentary Questions";
+            }
+            
+            if (remaining.TotalSeconds > 0)
+                lblTimer.Text = remaining.ToString("mm':'ss");
+            else
+            {
+                twoMinuteWarning();
+                if (round == 1)
+                {
+                    round = 2;
+                    btnContinue.Enabled = true;
+                    btnContinue.Text = "Continue";
+                }
+                timer1.Enabled = false;
+
+            }
+        }
+
+        //resets everything
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            round = 0;
+            lblRoundTitle.Visible = false;
+            duration = new TimeSpan(0, TOTAL_MINUTES, 1);
+            timer1.Enabled = false;
+            timer2.Enabled = false;
+            this.BackColor = SystemColors.Control;
+            btnContinue.Enabled = true;
+            lblTimer.Text = "10:00";
+            timerTickCount = 0;
+        }
+
+        private void twoMinuteWarning()
+        {
+            timerTickCount = 0;
+            timer2.Enabled = true;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (remaining.TotalSeconds <= 0)
+            {
+                if (this.BackColor == SystemColors.Control)
+                    this.BackColor = System.Drawing.Color.Red;
+                else
+                    this.BackColor = SystemColors.Control;
+
+                timerTickCount += 1;
+
+                if (timerTickCount >= 12)
+                    timer2.Enabled = false;
+            }
+            else
+            {
+                if (this.BackColor == SystemColors.Control)
+                    this.BackColor = System.Drawing.Color.Black;
+                else
+                    this.BackColor = SystemColors.Control;
+
+                timerTickCount += 1;
+
+                if (timerTickCount >= 10)
+                    timer2.Enabled = false;
+            }
+        }
+
+
+    }
+}
